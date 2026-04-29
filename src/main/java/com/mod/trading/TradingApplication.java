174b05fbfdeeaa7bd6b79544c2dce37d522977e6
iMOD07 <mo.2301@hotmail.com>
2026-04-29 15:04:18 +0300

    last IBKR

--- src/main/java/com/mod/trading/TradingApplication.java
+++ src/main/java/com/mod/trading/TradingApplication.java
@@ -8,5 +8,6 @@ public class TradingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TradingApplication.class, args);
+        System.out.println("last");
     }
 }
